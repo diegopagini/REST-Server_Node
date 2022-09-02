267453984112df4c2a7b957a1b6e3122c6496e9f
Diego Pagini <diego.pagini@neoris.com>
2022-09-02 14:50:06 +0200

    crud

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 /** @format */
 import { config } from 'dotenv';
-import express from 'express';
+
+import { Server } from './models/server.js';
 
 config();
-const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
-
-app.listen(process.env.PORT, () => {
-	console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
-});
+const server = new Server();
+server.listen();
---
+++ models/server.js
@@ -0,0 +1,52 @@
+/** @format */
+import express from 'express';
+
+export class Server {
+	constructor() {
+		this.app = express();
+		this.port = process.env.PORT;
+
+		// Middlewares
+		this.middlewares();
+		// Routes
+		this.routes();
+	}
+
+	middlewares() {
+		// "use" is the keyword to middlewares.
+		this.app.use(express.static('public'));
+	}
+
+	routes() {
+		// "get", "post", "put", "delete", etc. are the keywords to routes.
+		this.app.get('/api', (req, res) => {
+			res.status(200).json({
+				message: 'get API',
+			});
+		});
+
+		this.app.post('/api', (req, res) => {
+			res.status(201).json({
+				message: 'post API',
+			});
+		});
+
+		this.app.put('/api', (req, res) => {
+			res.status(400).json({
+				message: 'put API',
+			});
+		});
+
+		this.app.delete('/api', (req, res) => {
+			res.status(500).json({
+				message: 'delete API',
+			});
+		});
+	}
+
+	listen() {
+		this.app.listen(this.port, () => {
+			console.log(`server running in port: ${this.port}`);
+		});
+	}
+}
