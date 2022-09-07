0fafceb93a8217ad0f95e566cde98bbcfc284957
Diego Pagini <diego.pagini@neoris.com>
2022-09-07 14:59:56 +0200

    jwt validations

--- middlewares/validate-jwt.js
+++ middlewares/validate-jwt.js
@@ -2,7 +2,9 @@
 import { request, response } from 'express';
 import jwt from 'jsonwebtoken';
 
-export const validateJWT = (req = request, res = response, next) => {
+import { User } from '../models/user.js';
+
+export const validateJWT = async (req = request, res = response, next) => {
 	const token = req.header('x-token'); // To get an specific header.
 
 	if (!token)
@@ -11,9 +13,23 @@ export const validateJWT = (req = request, res = response, next) => {
 		});
 
 	try {
-		const { uid } = jwt.verify(token, process.env.SECRET_OR_PRIVATE_KEY); // TO verify if it is a valid token.
+		const { uid } = jwt.verify(token, process.env.SECRET_OR_PRIVATE_KEY); // To verify if it is a valid token.
 
-		req.uid = uid; // To add the uid in the request.
+		const user = await User.findById(uid);
+
+		// Check if is a valid user.
+		if (!user)
+			return res.status(401).json({
+				message: 'Not a valid user.',
+			});
+
+		// Check if the user has a true status.
+		if (!user.status)
+			return res.status(401).json({
+				message: 'Not a valid token.',
+			});
+
+		req.user = user; // To add the authenticated user to the request.
 
 		next(); // To continue with the next middleware.
 	} catch (error) {
