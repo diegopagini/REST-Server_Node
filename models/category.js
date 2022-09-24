b39b5762bcccbe77f437206efdb8b99b5b761bba
Diego Pagini <diegopaginidev@gmail.com>
2022-09-24 15:37:35 +0200

    Category Model

---
+++ models/category.js
@@ -0,0 +1,23 @@
+/** @format */
+import mongoose from 'mongoose';
+
+const { Schema, model } = mongoose;
+
+const CategorySchema = Schema({
+	name: {
+		type: String,
+		required: [true, 'Name is mandatory'],
+	},
+	state: {
+		type: Boolean,
+		default: true,
+		required: true,
+	},
+	user: {
+		type: Schema.Types.ObjectId, // Must be a user from the collection.
+		ref: 'User',
+		required: true,
+	},
+});
+
+export const Category = model('Category', CategorySchema);
